a4fc308635565d925d7431a0fa7f5c8368d535a6
Skuller <mateussukullerdev@gmail.com>
2026-07-24 22:24:37 -0300

    The name of in the money are only the numbers now

--- src-tauri/src/calc/calculate.rs
+++ src-tauri/src/calc/calculate.rs
@@ -30,64 +30,63 @@ pub fn calcular_troco(
         itens,
     })
 }
-
-#[tauri::command(rename_all = "snake_case")]
-pub fn calcular(valor_total: u32, valor_recebido: u32) -> Result<Resultado, String> {
+#[tauri::command]
+pub fn calcular(valor_total: u32, valor_recebido: u32) -> Resultado {
     // Lista padrão do Real Brasileiro (em centavos)
     let moedas_e_notas = vec![
         Denominacao {
             valor_em_centavos: 10000,
-            nome: "R$ 100".into(),
+            nome: "100".into(),
             eh_moeda: false,
         },
         Denominacao {
             valor_em_centavos: 5000,
-            nome: "R$ 50".into(),
+            nome: "50".into(),
             eh_moeda: false,
         },
         Denominacao {
             valor_em_centavos: 2000,
-            nome: "R$ 20".into(),
+            nome: "20".into(),
             eh_moeda: false,
         },
         Denominacao {
             valor_em_centavos: 1000,
-            nome: "R$ 10".into(),
+            nome: "10".into(),
             eh_moeda: false,
         },
         Denominacao {
             valor_em_centavos: 500,
-            nome: "R$ 5".into(),
+            nome: "5".into(),
             eh_moeda: false,
         },
         Denominacao {
             valor_em_centavos: 200,
-            nome: "R$ 2".into(),
+            nome: "2".into(),
             eh_moeda: false,
         },
         Denominacao {
             valor_em_centavos: 100,
-            nome: "R$ 1".into(),
+            nome: "1".into(),
             eh_moeda: true,
         },
         Denominacao {
             valor_em_centavos: 50,
-            nome: "R$ 0,50".into(),
+            nome: "0.50".into(),
             eh_moeda: true,
         },
         Denominacao {
             valor_em_centavos: 25,
-            nome: "R$ 0,25".into(),
+            nome: "0.25".into(),
             eh_moeda: true,
         },
         Denominacao {
             valor_em_centavos: 10,
-            nome: "R$ 0,10".into(),
+            nome: "0.10".into(),
             eh_moeda: true,
         },
         Denominacao {
             valor_em_centavos: 5,
-            nome: "R$ 0,05".into(),
+            nome: "0.05".into(),
             eh_moeda: true,
         },
     ];
@@ -98,9 +97,8 @@ pub fn calcular(valor_total: u32, valor_recebido: u32) -> Result<Resultado, Stri
     };
 
     match calcular_troco(input, moedas_e_notas) {
-        Ok(resultado) => Ok(resultado),
-        Err(CalcError::ValorInsuficiente) => Err("Valor insuficiente".to_string()),
-        Err(_) => Err("Ocorreu um erro inesperado".to_string()),
+        Ok(resultado) => resultado,
+        Err(_) => panic!("Ocorreu um erro"),
     }
 }
 
@@ -111,7 +109,7 @@ mod tests {
     fn test_troco() {
         let valor_total = 6400;
         let valor_recebido = 10000;
-        let res = calcular(valor_total, valor_recebido).unwrap();
+        let res = calcular(valor_total, valor_recebido);
         assert_eq!(res.valor_do_troco, 3600);
     }
 }
